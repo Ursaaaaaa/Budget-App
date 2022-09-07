3be52299d1cd6def02ef6fd1d050754ac626b721
ramrod433 <djoletoamanor@gmail.com>
2022-09-07 16:51:20 +0000

    Added payment specs

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Payments", type: :request do
-  describe "GET /payments" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Payments', type: :request do
+  describe 'payments' do
+    it 'GET /payments' do
+      get payments_path
+      expect(response).to render_template('index')
+    end
+
+    it 'GET /payments' do
       get payments_path
       expect(response).to have_http_status(200)
     end
