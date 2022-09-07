f5809965339905c53579edeae2ff3ff83fad4e0b
ramrod433 <djoletoamanor@gmail.com>
2022-09-07 13:39:50 +0000

    Added User relationship

---
+++ db/migrate/20220907133926_categories_payments.rb
@@ -0,0 +1,7 @@
+class CategoriesPayments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories_payments, :id => false do |t|
+      t.references :category, foreign_key: true
+      t.references :payment, foreign_key: true
+  end
+end
