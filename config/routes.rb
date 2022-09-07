cbd6df6c4864679d3620768aa7cef2bf943ad2c2
ramrod433 <djoletoamanor@gmail.com>
2022-09-07 13:46:27 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'payments/index'
-  get 'payments/new'
-  get 'payments/show'
-  get 'payments/edit'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/show'
-  get 'users/index'
+  resources :payments
+  resources :categories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
